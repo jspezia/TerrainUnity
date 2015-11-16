6775c99bd98eb89e84a9b7cf2f10ccf75c7008c6
Jean SPEZIA <jspezia@e3r9p19.42.fr>
2015-11-16 17:46:35 +0100

    p

---
+++ Assets/Scripts/Camera_position.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_position : MonoBehaviour {
+
+	public GameObject		Player;
+
+	private Vector3		_position;
+	
+	void Update () {
+		_position = Player.transform.position;
+		transform.position = new Vector3(_position.x, _position.y + 45f, _position.z - 37f);
+	}
+}
---
+++ Assets/Scripts/Rabbit_controler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rabbit_controler : MonoBehaviour {
+
+	private RaycastHit		_rayHit;
+	private Ray				_ray;
+	private NavMeshAgent	nav;
+
+	private Animator		anim;
+
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+		anim = GetComponent<Animator>();
+	}
+
+	void Update () {
+		//RAYCAST
+		_ray  = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Debug.DrawRay(_ray.origin, _ray.direction * 10f, Color.red);
+
+		if (Input.GetMouseButtonDown (0)) {
+			if (Physics.Raycast(_ray, out _rayHit)) {
+				Vector3		point = _rayHit.point;
+				nav.destination = point;
+			}
+		}
+	}
+}
